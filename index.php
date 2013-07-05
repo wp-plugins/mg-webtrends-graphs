82906756bd670b2c7d475fe94986382d3cc98de4
Rashef <Rashef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-05 06:44:38 +0000

    Tagging 0.2.1
    git-svn-id: https://plugins.svn.wordpress.org/mg-webtrends-graphs/tags/0.2.1@736554 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: mgtrends
+Plugin URI: http://www.mirkogrewing.eu/mg-webtrends-graphs/
+Description: This plugin provides a shortcode that allows to embed a Google WebTrends graphic in your website.
+Version: 0.2.1
+Author: Mirko Grewing
+Author URI: http://www.mirkogrewing.eu
+
+	Copyright: © 2013 Mirko Grewing (email : mirko@grewing.us)
+	License: GNU General Public License v3.0
+	License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+if ( ! defined( 'ABSPATH' ) )
+	die( "Can't load this file directly" );
+
+class MGTrends
+{
+	function __construct() {
+		add_action( 'admin_init', array( $this, 'action_admin_init' ) );
+	}
+	
+	function action_admin_init() {
+		// only hook up these filters if we're in the admin panel, and the current user has permission
+		// to edit posts and pages
+		if ( current_user_can( 'edit_posts' ) && current_user_can( 'edit_pages' ) ) {
+			add_filter( 'mce_buttons', array( $this, 'filter_mce_button' ) );
+			add_filter( 'mce_external_plugins', array( $this, 'filter_mce_plugin' ) );
+		}
+	}
+	
+	function filter_mce_button( $buttons ) {
+		// add a separation before our button, here our button's id is "mgtrends_button"
+		array_push( $buttons, '|', 'mgtrends_button' );
+		return $buttons;
+	}
+	
+	function filter_mce_plugin( $plugins ) {
+		// this plugin file will work the magic of our button
+		$plugins['mgtrends'] = plugin_dir_url( __FILE__ ) . 'mgtrends_plugin.js';
+		return $plugins;
+	}
+}
+
+function mg_trend($atts){
+	extract( shortcode_atts( array(
+			'w' => '500',           // width
+			'h' => '330',           // height
+			'q' => '',              // query
+			'loc' => 'US',          // location
+	), $atts ) );
+	//format input
+	$h=(int)$h;
+	$w=(int)$w;
+	$q=esc_attr($q);
+	$loc=esc_attr($loc);
+	$lan=str_replace("_","-",get_locale());
+	ob_start();
+	?>
+	<script type="text/javascript" src="http://www.google.com/trends/embed.js?hl=<?php echo $lan;?>&q=<?php echo $q;?>&geo=<?php echo $loc;?>&cmpt=q&content=1&cid=TIMESERIES_GRAPH_0&export=5&w=<?php echo $w;?>&h=<?php echo $h;?>"></script>
+	<?php
+	return ob_get_clean();
+}
+
+add_shortcode('mgtrends','mg_trend');
+$mygallery = new MGTrends();
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== MG Webtrends Graphs ===
+Contributors: rashef
+Donate link: http://cl.ly/0m1h2L3u0K3Y
+Tags: Google, Trends, graph, chart, shortcode
+Requires at least: 3.3
+Tested up to: 3.5.2
+Stable tag: 0.2.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin provides a shortcode that allows to embed a Google Trends graph in your website.
+
+== Description ==
+
+If you want to embed Google Trends graphs in your website this is the plugin for you: with a simple shortcode you will get the graph you need in a few seconds.  
+A nice wizard will help you choosing the proper options!  
+  
+Do you want to raise a bug, propose a change, request for support or simply congratulate me? The Official Page of the plugin is waiting for your voice!    
+  
+http://www.mirkogrewing.eu/woocommerce-frontend-inventory/     
+http://support.mirkogrewing.eu/   
+
+== Installation ==
+
+1. Upload the plugin's folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. While editing a document, click on the icon in the toolbar and fill in the text fields.
+1. Click on the "Insert" button to place the shortcode inside the page.
+
+== Frequently asked questions ==
+
+= Can I change the language? =
+
+At the moment the plugin tries using the Wordpress locale set in your `wp-config.php`.
+
+== Changelog ==
+
+= v0.2 =
+* Added the wizard to the toolbar.
+
+= v0.1 =
+* First release.
+
+== Upgrade notice ==
+
+Nothing you should worry about! ;)
